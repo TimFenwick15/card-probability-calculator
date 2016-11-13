1be1d4aca8919bd747ba0514ec93200159de4e2f
TimFenwick15 <timothyfenwick15@gmail.com>
2016-11-13 11:20:27 +0000

    GUI now hangles >1 target

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+/*
+ * Write any results requested
+ */
 function resolve(x) {
+  
+  // this logic needs fixing.
+  // x won't be an array, just targetsPlayed will be an array.
+  // Every other property is the same for each run
+  // This needs fixing in maths.js too because currently loops over x.length
+  
   if (x.constructor !== Array)
     x = [x]
 
   let err = new Set
   x.forEach(_ => {
-    if (!_.deckSize || !_.targetsPlayed || !_.cardsDrawn)
+    if (!_.deckSize || /*!_.targetsPlayed ||*/ !_.cardsDrawn)
       err.add('Complete the form :(')
-    if (_.targetsPlayed > _.deckSize || _.cardsDrawn > _.deckSize)
+    if (/*_.targetsPlayed > _.deckSize ||*/ _.cardsDrawn > _.deckSize)
       err.add('Logical fallacy detected. Git gud')
   })
   if (err.size)
@@ -16,21 +25,34 @@ function resolve(x) {
 }
 
 
-
+/*
+ * Event Handlers
+ */
 (function () {
+  // Handle the submit button
   document.getElementById('calc').addEventListener('click', event => {
     const params = {
-      /* Will be added later
-      upstart: document.getElementById('upstart').value === 'yes',
-      */
       cardsDrawn: Number(document.getElementById('draws').value),
-      targetsPlayed: Number(document.getElementById('played').value),
-      deckSize: Number(document.getElementById('deckSize').value)
+      deckSize: Number(document.getElementById('deckSize').value),
+      targetsPlayed: Array.from(document.querySelectorAll('#targetInputs'))
+        .map(_ => Number(_.value))
     }
 
     document.getElementById('results')
       .getElementsByTagName('p')[0]
       .textContent = resolve(params)
+  })
+
+  // Handle the add new target button
+  document.getElementById('addTarget').addEventListener('click', event => {
+    const newP = document.createElement('p')
+    newP.innerHTML = 'Number of Targets Played : '
+    const newInput = document.createElement('input')
+    newInput.id = 'targetInputs'
+    newInput.type = 'number'
+    newInput.value = ''
+    newP.appendChild(newInput)
+    document.getElementById('targetP').appendChild(newP)
   })
 })()
 
